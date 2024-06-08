6fb2f5e1c0c7c80849c342a04b545475d155c864
unknown <philippediniz@outlook.com>
2024-06-08 16:04:25 -0300

    Criando uma REST API

---
+++ src/main/java/dio_web_api/MyFirtsWebApiApplication.java
@@ -0,0 +1,13 @@
+package dio_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirtsWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirtsWebApiApplication.class, args);
+	}
+
+}
